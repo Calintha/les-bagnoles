005617632a880cefc79d27068964ccc0d426f16f
Maxime Carluer <mcarluer@myges.fr>
2023-03-17 16:13:52 +0100

    Feature [Car] - create entity, repository and fixtures

---
+++ src/repository/products/car_repository.js
@@ -0,0 +1,38 @@
+import CARS_DATA from '../../domain/products/cars.json' assert { type: 'json' };
+import { Car } from '../../entity/car.js';
+
+const CARS = CARS_DATA.map((car) => new Car(car));
+
+class CarRepository {
+    find = (id) => {
+        return CARS.find((car) => car.id === id);
+    }
+
+    findBy = (options = []) => {
+        return CARS.filter((car) => {
+            let result = true;
+
+            options.forEach((option) => {
+                const key = Object.keys(option)[0];
+                const value = option[key];
+
+                if (car[key] !== value) {
+                    result = false;
+                }
+            });
+
+            return result;
+        });
+    }
+
+    findOneBy = (options = []) => {
+        const result = this.findBy(options);
+        return result.length > 0 ? result[0] : null;
+    }
+
+    findAll = () => {
+        return CARS;
+    }
+}
+
+export default new CarRepository();
--- src/routes/product/[slug]/+page.js
+++ src/routes/product/[slug]/+page.js
@@ -1,5 +1,12 @@
+import CarRepository from '../../../repository/products/car_repository.js';
+
 export const load = ({ params }) => {
+    const car = CarRepository.findOneBy([
+        { 'slug': params.slug }
+    ]);
+
     return {
-        slug: params.slug
+        slug: params.slug,
+        car: car
     }
 }
