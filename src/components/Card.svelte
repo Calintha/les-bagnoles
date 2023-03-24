c10b5263c6cf337bb5a7bf055950bd27c6d0748c
calintha <cal.intha77@gmail.com>
2023-03-24 13:37:25 +0100

    Feature [Front] - add cards

---
+++ src/components/Card.svelte
@@ -0,0 +1,57 @@
+<style>
+.card {
+    width: 20%;
+    border: 1px solid #dee2e6;
+    border-radius: 0.25rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
+    overflow: hidden;
+    transition: transform 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.card-body {
+    padding: 1rem;
+    text-align: center;
+}
+
+.card-title {
+    font-size: 1.25rem;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+}
+
+.card-text {
+    font-size: 1rem;
+    line-height: 1.5;
+}
+
+.icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
+    height: 60px;
+    font-size: 2rem;
+    color: #fff;
+    background-color: #007bff;
+    border-radius: 50%;
+    margin-right: 1rem;
+}
+</style>
+
+<script>
+    export let title;
+    export let description;
+    export let icon;
+</script>
+
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex">
+            <div class="icon">{icon}</div>
+            <div class="content">
+                <h2 class="card-title">{title}</h2>
+                <p class="card-text">{description}</p>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,47 @@
-<h1>Homepage</h1>
+<script>
+    import Card from '../components/Card.svelte';
+
+    let cards = [
+        {
+            title: 'Verify seller',
+            description: 'For a safe purchase, identify the validated and certified sellers by The Bagnols',
+            icon: 'none'
+        },
+        {
+            title: 'History details',
+            description: 'Trace with confidence the history of each vehicle',
+            icon: 'none'
+        },
+        {
+            title: 'Delivery',
+            description: 'Your new vehicle delivered to your door thanks to our delivery service',
+            icon: 'none'
+        },
+        {
+            title: 'Assurance',
+            description: 'Protect your future vehicle by simulating your insurance for free',
+            icon: 'none'
+        }
+    ]
+</script>
+
+<h1>Katchaaaaaaaaaaaaaaaaaaaaaaw !</h1>
+<p>Hope you find your soulmate</p>
+
+<div class="cards">
+    {#each cards as card}
+        <Card
+            title={card.title}
+            description={card.description}
+            icon={card.icon}
+        />
+    {/each}
+</div>
+
+<style>
+    .cards {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
