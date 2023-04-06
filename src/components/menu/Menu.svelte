cfb56cf5f471001828eb3e31993009740be76eba
Maxime Carluer <mcarluer@myges.fr>
2023-04-06 16:42:02 +0200

    Feature [Style] - add style for homepage

--- src/components/menu/Menu.svelte
+++ src/components/menu/Menu.svelte
@@ -5,3 +5,23 @@
         <li><a href="/profile">Profile</a></li>
     </ul>
 </nav>
+
+<style>
+    nav {
+        background-color: #f8f9fa;
+        padding: 1rem;
+    }
+    nav ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        align-items: center;
+    }
+    nav ul li {
+        list-style: none;
+    }
+    nav ul li a {
+        color: #000;
+        text-decoration: none;
+    }
+</style>
