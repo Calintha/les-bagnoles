fbce70bb61bc9c682dd9f11f3e39f5c225a6d733
calintha <cal.intha77@gmail.com>
2023-04-12 14:52:58 +0200

    Feature [Video] - webp for video

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -13,10 +13,27 @@
         width: 100%;
         height: 100%;
     }
+
+    .video-overlay {
+        position: absolute;
+        top: 50%;
+        right: 0;
+        color: #fff;
+        margin: 0;
+        padding: 10px;
+        font-weight: bold;
+        transform: translateY(50%);
+        font-size: 2em;
+        text-align: right;
+        margin-right: 15px;
+        background-color: rgba(0, 0, 0, 0.5);
+        box-sizing: border-box;
+    }
 </style>
 <div class="video-container">
     <video controls>
+        <source src="home/video.webp" type="video/webp">
         <source src="home/video.mp4" type="video/mp4">
-        <source src="home/video.mp4" type="video/webm">
     </video>
+    <div class="video-overlay">Calvin</div>
 </div>
